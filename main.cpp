0d3815cbf95119be55fd4f38e31298a2f83138d1
EricWRogers <yerick147083@gmail.com>
2018-02-24 19:18:35 -0600

    drawing am square efficiently

--- main.cpp
+++ main.cpp
@@ -107,15 +107,29 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float position[6] = {-0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f};
+    float position[] = {
+        -0.5f, -0.5f, 
+        0.5f, -0.5f, 
+        0.5f, 0.5f,
+        -0.5f, 0.5f,
+    };
 
+    unsigned int indices[] ={
+        0, 1, 2,
+        2, 3, 0
+    };
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), position, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), position, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
@@ -130,7 +144,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         //Drawing a 2d triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
